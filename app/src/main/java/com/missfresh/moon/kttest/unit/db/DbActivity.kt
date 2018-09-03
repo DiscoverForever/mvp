e2ef737261d663021ab81caf4ce7c584dd8a34cf
tangyueyang <yueyangtang@163.com>
2018-09-03 22:43:08 +0800

    Add GreenDAO集成完毕

--- app/src/main/java/com/missfresh/moon/kttest/unit/db/DbActivity.kt
+++ app/src/main/java/com/missfresh/moon/kttest/unit/db/DbActivity.kt
@@ -1,10 +1,15 @@
 package com.missfresh.moon.kttest.unit.db
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Toast
 import com.missfresh.moon.kttest.base.BaseActivity
+import com.missfresh.moon.kttest.crud.UserDaoOpe
+import com.missfresh.moon.kttest.entity.user.User
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
@@ -12,8 +17,10 @@ class DbActivity() : BaseActivity(), DbContract.View {
     override var presenter: DbPresenter = DbPresenter(this)
     private lateinit var etName: EditText
     private lateinit var etAge: EditText
+    private lateinit var mContext: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mContext = this
         verticalLayout() {
             linearLayout() {
                 weightSum = 3f
@@ -56,7 +63,12 @@ class DbActivity() : BaseActivity(), DbContract.View {
                 rightMargin = dip(30)
             }
             button("新增") {
-                onClick { }
+                onClick {
+                    var user = User()
+                    user.name = etName.text?.toString()
+                    user.age = etAge.text?.toString()
+                    UserDaoOpe.getInstance().insertData(mContext, user)
+                }
             }
             button("删除") {
                 onClick { }
@@ -64,8 +76,12 @@ class DbActivity() : BaseActivity(), DbContract.View {
             button("修改") {
                 onClick { }
             }
-            button("删除") {
-                onClick { }
+            button("查询") {
+                onClick {
+                    var user = UserDaoOpe.getInstance().queryById(mContext, 1)
+                    Log.d("name", user?.name)
+                    Log.d("age", user?.age)
+                }
             }
         }
 
